f2b5526f6da671f5720f54ab5a1a956ccc00342e
nanarino <49119482+nanarino@users.noreply.github.com>
2019-12-10 16:34:39 +0800

    MOD        批量拖拽上传

--- static/relaxation/js/addAlbum.js
+++ static/relaxation/js/addAlbum.js
@@ -1,89 +1,122 @@
 $(document).ready(function(){
-
-    let uploadList=[]
-    let canupList=["image/gif","image/jpeg","image/png"]
-    $("#upload")[0].ondragover = function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        return false;
-    };
-    $("#upload")[0].ondrop = function (e) {
-        // 1.file对象
-        const file = e.dataTransfer.files.item(0);
-        if(uploadList.length>=9){return false;}
-        if (canupList.indexOf(file.type)===-1){return false;}
-        uploadList.push(file)
-        console.log(uploadList)
-
-        // 创建文件读取对象
-        const f = new FileReader();
-        // 读取图片 格式base64
-        f.readAsDataURL(file);
-        f.onload = function(e) {
-            const img = new Image();
-            // 格式base64
-            img.src = this.result;
-            img.style.width="20%"
-            upload.appendChild(img);
-        };
-        e.preventDefault();
-        e.stopPropagation();
-        return false;
-    }
-
-	function checkaddAlbum(){
-		if($("#album-title").val().trim()===""){
-			$(".focusbox-title").html('标题不能为空')
-            rewardsON()
-            rewardsLog('发表失败','标题不能为空')
-			return false;
-		}
-		if(uploadList.length<1){
-			$(".focusbox-title").html('图集不能为空')
-			rewardsON()
-            rewardsLog('发表失败','图集不能为空')
-			return false;
-		}
-        var form_data = new FormData();
-        form_data.append('title', $("#album-title").val().trim());
-        form_data.append('motif', $("#album-motif").val().trim());
-        form_data.append('length',uploadList.length);
-        uploadList.forEach(function(v,i){
-            form_data.append('file'+i, v);
-        })
-        $('#artc-submit').off("click").css('cssText','background-color:#ABABA4;border-color:#ABABA4;');
-        rewardsON()
-        rewardsLog("正在上传","请耐心等待")
-        $.ajax({
-            type: "POST",  
-            url:location.pathname,  
-            data:form_data,  
-            async:true,
-                processData: false,  // tell jquery not to process the data
-                contentType: false,   // tell jquery not to set contentType         
-            error: function(request) {  
-                rewardsON()
-                rewardsLog("上传失败","可能是您的网络原因")
-            },  
-            success: function(data) {  
-                rewardsON()
-                if (data.msg===true){
-                    rewardsLog("发表成功","页面即将跳转:<span>3</span>")
-                    countDown(3, "/picture/")
-                }else if(data.msg===false){
-                    rewardsLog("发表失败","请检查图片张数")
-                }else if(data.msg==="error"){
-                    rewardsLog("上传出错","图片储存过程出错")
-                }else if(data.msg==="not_login"){
-                    rewardsLog("请先登录","页面即将跳转:<span>3</span>")
-                    countDown(3, "/user/login/?next="+location.pathname)
-                }else{
-                    rewardsLog("发表失败","可能是服务器抽风")
+    Vue.prototype.$ = window.$;
+    const vm = new Vue({
+        el: "#app",
+        data(){
+            return {
+                title: '',
+                motif: '',
+                maxNumber: 9,
+                imgList: [],
+                filterList: ["image/gif","image/jpeg","image/png","image/x-icon"],
+                size: 0 
+            }
+        },
+        methods: {
+            dragenter(el){
+                el.stopPropagation();
+                el.preventDefault();
+            },
+            dragover(el){
+                el.stopPropagation();
+                el.preventDefault();
+            },
+            drop(el){
+                el.stopPropagation();
+                el.preventDefault();  
+                if(this.imgList.length + el.dataTransfer.files.length > this.maxNumber){
+                    rewardsON()
+                    rewardsLog('操作失败',"已经超出张数！！！")
+                    return;
+                }else{                
+                    this.filesList(el.dataTransfer.files);
+                }      
+            },
+            filesList(files){
+                [...files].filter((v)=>this.filterList.indexOf(v.type)!==-1).forEach((v)=>this.fileAdd(v))
+            },
+            fileAdd(file){
+                this.size += file.size;
+                let reader = new FileReader();
+                reader.readAsDataURL(file);
+                reader.onload = ()=>{
+                    file.src = reader.result;
+                    this.imgList.push({file});
                 }
-            }  
-        });
-	}
-	$('#artc-submit').on('click', function(){
-		checkaddAlbum()
-	})
+            },
+            updateImg(el){
+                if(!el.target.files[0].size) return;
+                if(this.imgList.length >= this.maxNumber){
+                    rewardsON()
+                    rewardsLog('操作失败',"已经超出张数！！！")  
+                    return;
+                }else{
+                    this.filesList(el.target.files);
+                }     
+            },
+            delImg(index){
+                this.size -= this.imgList[index].file.size;
+                this.imgList.splice(index, 1);
+            },
+            postData(){
+                if(!this.title.trim()){
+                    rewardsON()
+                    rewardsLog('发表失败',"当前标题为空请输入标题！")
+                    return;
+                }
+                if(this.imgList.length < 1){
+                    rewardsON()
+                    rewardsLog('发表失败',"请至少选择一张图片上传")
+                    return;
+                }
+                var formData = new FormData();  
+                this.imgList.forEach((value, index) => {
+                    formData.append("file" + index, value.file);
+                });
+                formData.append("length", this.imgList.length);
+                formData.append("title", this.title);
+                formData.append("motif", this.motif); 
+                this.$('#artc-submit').off("click").css('cssText','background-color:#ABABA4;border-color:#ABABA4;');
+                rewardsON()
+                rewardsLog("正在上传","请耐心等待")
+                this.$.ajax({
+                    type: "POST",
+                    url:location.pathname,  
+                    data:formData,  
+                    async:true,
+                        processData: false,
+                        contentType: false,      
+                    error: function(request) {  
+                        rewardsON()
+                        rewardsLog("上传失败","可能是您的网络原因")
+                    },  
+                    success: function(data) {  
+                        rewardsON()
+                        if (data.msg===true){
+                            rewardsLog("发表成功","页面即将跳转:<span>3</span>")
+                            countDown(3, "/picture/")
+                        }else if(data.msg===false){
+                            rewardsLog("发表失败","请检查图片张数")
+                        }else if(data.msg==="error"){
+                            rewardsLog("上传出错","图片储存过程出错")
+                        }else if(data.msg==="not_login"){
+                            rewardsLog("请先登录","页面即将跳转:<span>3</span>")
+                            countDown(3, "/user/login/?next="+location.pathname)
+                        }else{
+                            rewardsLog("发表失败","可能是服务器抽风")
+                        }
+                    }  
+                });
+            } 
+        },
+        computed: {
+            bySize(){
+                if (this.size === 0) return '0 B';
+                let k = 1024, 
+                    sizes = ['B', 'KB', 'MB', 'GB'],
+                    i = Math.floor(Math.log(this.size ) / Math.log(k));
+                return (this.size  / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
+            }
+        }
+    })
 })
